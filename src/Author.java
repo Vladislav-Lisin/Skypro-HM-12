dbe47a0ed8af332c07038977e99a0d39ce11ac4d
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-08-03 08:46:40 +0300

    commit

---
+++ src/Author.java
@@ -0,0 +1,31 @@
+public class Author {
+    private String name;
+    private String surname;
+    private String fullname;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+
+}

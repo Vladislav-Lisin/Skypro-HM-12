1d067a9debfcb1e5a772ccc5e2def55777eab034
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-08-06 01:07:38 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author levTolstoi = new Author();
-        levTolstoi.setName("Лев");
-        levTolstoi.setSurname("Толстой");
-        levTolstoi.setFullname("Лев Николаевич Толстой");
-        System.out.println(levTolstoi.getName() + " " + levTolstoi.getSurname());
-        System.out.println(levTolstoi.getFullname());
+        Author levTolstoi = new Author("Лев", "Толстой");
+        Book warAndPeace = new Book("Война и мир", levTolstoi, 1869);
 
-
-        System.out.println();
-
-
-        Book warAndPeace = new Book();
-        warAndPeace.setYear(1867);
-        warAndPeace.setAuthor("Лев Толстой");
-        warAndPeace.setTitle("Война и мир");
         System.out.println(warAndPeace.getYear());
         System.out.println(warAndPeace.getTitle());
-        System.out.println(warAndPeace.getAuthor());
+        System.out.println(warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getSurname());
 
 
         System.out.println();
 
 
-        Author mari = new Author();
-        mari.setName("Мария (Пауль)");
-        mari.setSurname("Ремарк");
-        mari.setFullname("Эрих Мария Ремарк");
-        System.out.println(mari.getName() + " " + mari.getSurname());
-        System.out.println(mari.getFullname());
+        Author dostoevsk = new Author("Фёдор", "Достоевский");
 
-        Book threeLoads = new Book();
-        threeLoads.setYear(1935);
-        threeLoads.setAuthor("Эрих Мария Ремарк");
-        threeLoads.setTitle("Три товарища");
+        System.out.println(dostoevsk.getName() + " " + dostoevsk.getSurname());
+
+        Book threeLoads = new Book("Преступление и наказание", dostoevsk, 1866);
+        threeLoads.setYear(1967);
         System.out.println(threeLoads.getYear());
         System.out.println(threeLoads.getTitle());
-        System.out.println(threeLoads.getAuthor());
+        System.out.println(threeLoads.getAuthor().getName() + " " + threeLoads.getAuthor().getSurname());
 
-        threeLoads.setYear(1936);
+        threeLoads.setYear(1966);
         System.out.println(threeLoads.getYear());
     }
 }
